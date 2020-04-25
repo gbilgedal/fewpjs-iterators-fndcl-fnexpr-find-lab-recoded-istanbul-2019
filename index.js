aa1fa972c6bd797a27e0d24ec2eaf6b2e0eb32d8
G Bilge Dal <lagala999@gmail.com>
2020-04-25 08:34:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superBowlWin(arr){
+  arr.find(element =>{
+    if(element.result === "W"){
+      return element.year;
+    }
+    else{
+      return undefined;
+    }
+
+    
+  })
+  
+}
